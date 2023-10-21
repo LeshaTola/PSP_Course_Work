19ea9b530c36e0cd16c880114f4cb57e09b3313e
LeshaTola <tola.lesha@mail.ru>
2023-10-21 16:46:39 +0300

    Add Server projet

---
+++ Server/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketSellerLib.DTO;
+
+namespace TicketSeller.Model
+{
+	class ApplicationContext : DbContext
+	{
+		public DbSet<User> Users { get; set; } = null!;
+		public DbSet<Hall> Halls { get; set; } = null!;
+		public DbSet<Cinema> Cinema { get; set; } = null!;
+		public DbSet<Film> Films { get; set; } = null!;
+		public DbSet<Session> Session { get; set; } = null!;
+
+		public ApplicationContext()
+		{
+			Database.EnsureDeleted();
+			Database.EnsureCreated();
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql("Host=localhost;Port=8888;Database=Users;Username=postgres;Password=admin");
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using TicketSeller.Model;
+using TicketSellerLib.DTO;
+
+namespace Server
+{
+	public class Program
+	{
+
+		static void Main(string[] args)
+		{
+			AddUser(new User()
+			{
+				Login = "admin",
+				Password = "admin",
+				Email = "Tola.Lesha@mail.ru"
+			});
+		}
+
+		public static void AddUser(User user)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Users.Add(user);
+				db.SaveChanges();
+			}
+		}
+	}
+}

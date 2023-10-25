9170f7612846ded293c61776e1bcd9d1c60c24de
LeshaTola <tola.lesha@mail.ru>
2023-10-25 19:00:52 +0300

    Add DAO for database

---
+++ Server/DAO/CinemaDAO.cs
@@ -0,0 +1,51 @@
+﻿using TicketSeller.Model;
+using TicketSellerLib.DTO;
+
+namespace Server.DAO
+{
+	internal class CinemaDAO : DAO<Cinema>
+	{
+		public override void Add(Cinema item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Cinema.Add(item);
+				db.SaveChanges();
+			}
+		}
+
+		public override Cinema Get(int id)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				return db.Cinema.FirstOrDefault(c => c.Id == id);
+			}
+		}
+
+		public override List<Cinema> GetAll()
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				return db.Cinema.ToList();
+			}
+		}
+
+		public override void Remove(Cinema item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Cinema.Remove(item);
+				db.SaveChanges();
+			}
+		}
+
+		public override void Update(Cinema item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Cinema.Update(item);
+				db.SaveChanges();
+			}
+		}
+	}
+}
---
+++ Server/DAO/FilmDAO.cs
@@ -0,0 +1,51 @@
+﻿using TicketSeller.Model;
+using TicketSellerLib.DTO;
+
+namespace Server.DAO
+{
+	public class FilmDAO : DAO<Film>
+	{
+		public override void Add(Film item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Films.Add(item);
+				db.SaveChanges();
+			}
+		}
+
+		public override Film Get(int id)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				return db.Films.FirstOrDefault(f => f.Id == id);
+			}
+		}
+
+		public override List<Film> GetAll()
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				return db.Films.ToList();
+			}
+		}
+
+		public override void Remove(Film item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Films.Remove(item);
+				db.SaveChanges();
+			}
+		}
+
+		public override void Update(Film item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Films.Update(item);
+				db.SaveChanges();
+			}
+		}
+	}
+}

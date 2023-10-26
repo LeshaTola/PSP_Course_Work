9b75745dddcdba639d85b48ee698f45c66dce920
LeshaTola <tola.lesha@mail.ru>
2023-10-26 17:53:34 +0300

    Add Session DAO and Service

---
+++ Server/DAO/SessionDAO.cs
@@ -0,0 +1,51 @@
+﻿using TicketSeller.Model;
+using TicketSellerLib.DTO;
+
+namespace Server.DAO
+{
+	public class SessionDAO : DAO<Session>
+	{
+		public override void Add(Session item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Sessions.Add(item);
+				db.SaveChanges();
+			}
+		}
+
+		public override Session Get(int id)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				return db.Sessions.FirstOrDefault(s => s.Id == id);
+			}
+		}
+
+		public override List<Session> GetAll()
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				return db.Sessions.ToList();
+			}
+		}
+
+		public override void Remove(Session item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Sessions.Remove(item);
+				db.SaveChanges();
+			}
+		}
+
+		public override void Update(Session item)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Sessions.Update(item);
+				db.SaveChanges();
+			}
+		}
+	}
+}
---
+++ Server/Services/SessionService.cs
@@ -0,0 +1,34 @@
+﻿using Server.DAO;
+using TicketSellerLib.DTO;
+
+namespace Server.Services
+{
+	public class SessionService : Service<Session>
+	{
+		private SessionDAO dao = new SessionDAO();
+		public override void Add(Session item)
+		{
+			dao.Add(item);
+		}
+
+		public override Session Get(int id)
+		{
+			return dao.Get(id);
+		}
+
+		public override List<Session> GetAll()
+		{
+			return dao.GetAll();
+		}
+
+		public override void Remove(Session item)
+		{
+			dao.Remove(item);
+		}
+
+		public override void Update(Session item)
+		{
+			dao.Update(item);
+		}
+	}
+}

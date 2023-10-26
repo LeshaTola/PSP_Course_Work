015be438744ba58de9f4178954971a77c22777e7
LeshaTola <tola.lesha@mail.ru>
2023-10-26 13:19:30 +0300

    Add services

---
+++ Server/Services/CinemaService.cs
@@ -0,0 +1,34 @@
+﻿using Server.DAO;
+using TicketSellerLib.DTO;
+
+namespace Server.Services
+{
+	public class CinemaService : Service<Cinema>
+	{
+		private CinemaDAO dao = new CinemaDAO();
+		public override void Add(Cinema item)
+		{
+			dao.Add(item);
+		}
+
+		public override Cinema Get(int id)
+		{
+			return dao.Get(id);
+		}
+
+		public override List<Cinema> GetAll()
+		{
+			return dao.GetAll();
+		}
+
+		public override void Remove(Cinema item)
+		{
+			dao.Remove(item);
+		}
+
+		public override void Update(Cinema item)
+		{
+			dao.Update(item);
+		}
+	}
+}
---
+++ Server/Services/HallService.cs
@@ -0,0 +1,34 @@
+﻿using Server.DAO;
+using TicketSellerLib.DTO;
+
+namespace Server.Services
+{
+	public class HallService : Service<Hall>
+	{
+		private HallDAO dao = new HallDAO();
+		public override void Add(Hall item)
+		{
+			dao.Add(item);
+		}
+
+		public override Hall Get(int id)
+		{
+			return dao.Get(id);
+		}
+
+		public override List<Hall> GetAll()
+		{
+			return dao.GetAll();
+		}
+
+		public override void Remove(Hall item)
+		{
+			dao.Remove(item);
+		}
+
+		public override void Update(Hall item)
+		{
+			dao.Update(item);
+		}
+	}
+}
